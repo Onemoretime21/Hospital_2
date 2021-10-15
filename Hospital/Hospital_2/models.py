a0a81da39f05eebbbd871aa60090fdb7f4c8ccca
SadNurik <nuriksadybakasov@gmail.com>
2021-10-15 20:08:59 +0600

    fixed model

--- Hospital/Hospital_2/models.py
+++ Hospital/Hospital_2/models.py
@@ -13,12 +13,3 @@ class Hospitals(models.Model):
 
     def __str__(self):
         return self.hospital
-=======
-# Create your models here.
-
-class LatestNews(models.Model):
-    title = models.CharField(max_length=100)
-    create_at = models.DateTimeField(auto_now_add=True)
-    image = models.ImageField(upload_to='images/%Y/%m/%d/')
-    is_published = models.BooleanField(default=True)
->>>>>>> 4a9444ef282364627b79fc5e00fabf5dd8e41f12
