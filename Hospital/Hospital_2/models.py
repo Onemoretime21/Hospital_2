2f9909452c9905ab57aad09988f049ec7f5f7eb3
SadNurik <nuriksadybakasov@gmail.com>
2021-10-15 20:01:54 +0600

    create model

--- Hospital/Hospital_2/models.py
+++ Hospital/Hospital_2/models.py
@@ -1,3 +1,14 @@
+from django.conf import settings
 from django.db import models
 
-# Create your models here.
+class Hospitals(models.Model):
+    hospital = models.CharField(max_length=200)
+    main_doctor = models.CharField(max_length=200)
+    hirurg = models.CharField(max_length=200)
+    terapevt = models.CharField(max_length=200)
+    medsestra = models.CharField(max_length=200)
+    pasienty = models.CharField(max_length=200)
+
+
+    def __str__(self):
+        return self.hospital
