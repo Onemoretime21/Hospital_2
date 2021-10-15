52b8362a04f354b777c2f47e5100332de1ed7b8a
Onemoretime21 <s.saydinov@gmail.com>
2021-10-15 19:56:49 +0600

    Adaptive web-site

--- Hospital/Hospital_2/models.py
+++ Hospital/Hospital_2/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
 # Create your models here.
+
+class LatestNews(models.Model):
+    title = models.CharField(max_length=100)
+    create_at = models.DateTimeField(auto_now_add=True)
+    image = models.ImageField(upload_to='images/%Y/%m/%d/')
+    is_published = models.BooleanField(default=True)
--- Hospital/Hospital_2/views.py
+++ Hospital/Hospital_2/views.py
@@ -1,3 +1,12 @@
 from django.shortcuts import render
+from .models import *
 
 # Create your views here.
+
+
+def index(request):
+    ln = LatestNews.objects.all()
+    context = {
+        "qwe": ln
+    }
+    return render(request, template_name='Hospital_2/index.html', context=context)
